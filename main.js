1361061c9f8c17faa660c448e32d5230862a2ebd
lanaba99 <lanabarakat74@gmail.com>
2024-06-13 14:28:10 +0300

    change color

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 const geometry = new T.BoxGeometry(1, 1, 1)
 const material = new T.MeshBasicMaterial({
-    color: 0x00ff00,
+    color: 0x774422,
 })
 const cube = new T.Mesh(geometry, material)
 scene.add(cube)
