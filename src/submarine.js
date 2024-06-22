f51151cd11d3c9a608de767316006121ea5fe44d
FadyDhaim <fadyaldhaim@gmail.com>
2024-06-22 14:51:40 +0300

    zx

--- src/submarine.js
+++ src/submarine.js
@@ -18,10 +18,14 @@ export class Submarine {
             linear: 0.0,
             angular: 0.0
         }
-        this.maximumForwardSpeed = 30.0
         this.direction = directions.idle
-        this.holdTimeInFrames = 0
-        this.updateHoldTime = this.updateHoldTime.bind(this)
+        this.maximumForwardSpeed = 10.0
+        this.maximumReverseSpeed = -5.0
+        this.holdTime = 0 //in frames🖼️
+        this.maximumForwardHoldTime = 180  //3 seconds worth of frames @60 FPS
+        this.maximumReverseHoldTime = -180
+        this.pushHoldTimeAhead = this.pushHoldTimeAhead.bind(this)
+        this.pushHoldTimeBackward = this.pushHoldTimeBackward.bind(this)
         this.updateDirection = this.updateDirection.bind(this)
     }
     async load() {
@@ -41,9 +45,12 @@ export class Submarine {
 
     animate(time) {
         // console.log(this.direction)
+        if (this.direction == directions.idle && this.holdTime != 0) {
+            this.dampenHoldTime()
+        }
+        const acceleration = this._holdTimeToForwardAcceleration()
         const { direction, velocity } = this
         const { linear, angular } = velocity
-        const acceleration = this._holdTimeToForwardAcceleration()
         let newLinearVelocity
         switch (direction) {
             case directions.forward:
@@ -57,43 +64,78 @@ export class Submarine {
             this.submarineMesh.position.z -= newLinearVelocity
         }
     }
-    _holdTimeToForwardAcceleration() {
-        const holdTimeInFrames = this.holdTimeInFrames
-        const maximumSpeed = this.maximumForwardSpeed
+    _holdTimeToAcceleration() {
         let acceleration = 0
-        if (holdTimeInFrames > 0) {
-            if (holdTimeInFrames <= 60) {
-                // return 0.002    //0.002 * 60  = 0.12    == 20% of maxium speed (0.6) in one second
-                acceleration = (0.2 * maximumSpeed) / 60
-            }
-            else if (holdTimeInFrames <= 120) {
-                // return 0.003    //first-second-speed + second-second-speed = 0.12 + 0.003 * 60 = 0.3  == 50% of maxium speed
-                //                                                                                                           20%Max + X * 60 = 50% MAx => X * 60 = 30% Max => X = 30% / 60
-                acceleration = (0.3 * maximumSpeed) / 60
-            }
-            else {
-                // return 0.004    //0.3 + third-second = 0.3 + 0.004 * 60 = 0.54,  (90% of maximum speed in 3 seconds)
-                //50%Max + X * 60 = 90% Max => X * 60 = 40% Max   => X = 40% Max / 60
-                acceleration = (0.4 * maximumSpeed) / 60
-            }
+        const holdTime = this.holdTime
+        if (holdTime > 0) {
+            acceleration = this._holdTimeToForwardAcceleration()
+        } else if (holdTime < 0) {
+
+        }
+        return acceleration
+    }
+    _holdTimeToForwardAcceleration() {  //only called when holdTime is positive, ie, going forward
+        const holdTime = this.holdTime
+        const maximumSpeed = this.maximumForwardSpeed
+        const phase1 = (1 / 3) * this.maximumForwardHoldTime
+        const phase2 =(2 / 3) * this.maximumForwardHoldTime
+        const phase3 = (3 / 3) * this.maximumForwardHoldTime
+        let acceleration = 0
+        if (holdTime <= phase1) {
+            // return 0.002    //0.002 * 60  = 0.12    == 20% of maxium speed (0.6) in one second
+            acceleration = (0.2 * maximumSpeed) / 60
+        }
+        else if (holdTime <= phase2) {
+            // return 0.003    //first-second-speed + second-second-speed = 0.12 + 0.003 * 60 = 0.3  == 50% of maxium speed
+            //                                                                                                           20%Max + X * 60 = 50% MAx => X * 60 = 30% Max => X = 30% / 60
+            acceleration = (0.3 * maximumSpeed) / 60
+        }
+        else if (holdTime <= phase3){
+            // return 0.004    //0.3 + third-second = 0.3 + 0.004 * 60 = 0.54,  (90% of maximum speed in 3 seconds)
+            //50%Max + X * 60 = 90% Max => X * 60 = 40% Max   => X = 40% Max / 60
+            acceleration = (0.4 * maximumSpeed) / 60
+        }
+        return acceleration
+    }
+    _holdTimeToReverseAcceleration() {  //only called when holdTime is positive, ie, going forward
+        const holdTime = this.holdTime
+        const maximumSpeed = this.maximumReverseSpeed
+        const phase1 = (1 / 3) * this.maximumForwardHoldTime
+        const phase2 = (2 / 3) * this.maximumForwardHoldTime
+        const phase3 = (3 / 3) * this.maximumForwardHoldTime
+        let acceleration = 0
+        if (holdTime >= -60) {
+            // return 0.002    //0.002 * 60  = 0.12    == 20% of maxium speed (0.6) in one second
+            acceleration = (0.2 * maximumSpeed) / 60
+        }
+        else if (holdTime >= -) {
+            // return 0.003    //first-second-speed + second-second-speed = 0.12 + 0.003 * 60 = 0.3  == 50% of maxium speed
+            //                                                                                                           20%Max + X * 60 = 50% MAx => X * 60 = 30% Max => X = 30% / 60
+            acceleration = (0.3 * maximumSpeed) / 60
+        }
+        else if (holdTime <= this.maximumForwardHoldTime){
+            // return 0.004    //0.3 + third-second = 0.3 + 0.004 * 60 = 0.54,  (90% of maximum speed in 3 seconds)
+            //50%Max + X * 60 = 90% Max => X * 60 = 40% Max   => X = 40% Max / 60
+            acceleration = (0.4 * maximumSpeed) / 60
         }
         return acceleration
     }
     _setupInteractivity() {
-        const updateHoldTimeCallback = this.updateHoldTime
+        const pushHoldTimeAheadCallback = this.pushHoldTimeAhead
+        const pushHoldTimeBackwardCallback = this.pushHoldTimeBackward
         const updateDirectionCallback = this.updateDirection
         window.addEventListener('keydown', function (event) {
             const key = event.key
             switch (key) {
                 case 'ArrowUp':
-                    console.log('arrow up')
                     updateDirectionCallback(directions.forward)
+                    pushHoldTimeAheadCallback()
                     break
                 case 'ArrowDown':
-                    console.log('arrow downn')
                     updateDirectionCallback(directions.reverse)
+                    pushHoldTimeBackwardCallback()
+                    break
             }
-            updateHoldTimeCallback(1)
         })
         window.addEventListener('keyup', function (event) {
             const key = event.key
@@ -105,8 +147,22 @@ export class Submarine {
             }
         })
     }
-    updateHoldTime(valueOffset) {
-        this.holdTimeInFrames += valueOffset
+    pushHoldTimeAhead() {
+        if (this.holdTime < this.maximumForwardHoldTime) {
+            this.holdTime++
+        }
+    }
+    pushHoldTimeBackward() {
+        if (this.holdTime > this.maximumReverseHoldTime) {
+            this.holdTime--
+        }
+    }
+    dampenHoldTime() {
+        if (this.holdTime > 0) {
+            this.holdTime--
+        } else if (this.holdTime < 0) {
+            this.holdTime++
+        }
     }
     updateDirection(newDirection) {
         this.direction = newDirection
