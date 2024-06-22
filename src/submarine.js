117206f483c781509c1edfb916b31cc708aa0b8a
FadyDhaim <fadyaldhaim@gmail.com>
2024-06-22 16:29:06 +0300

    mn

--- src/submarine.js
+++ src/submarine.js
@@ -37,7 +37,7 @@ export class Submarine {
         return new Promise((resolve, reject) => {
             loader.load(this.modelPath, (gltf) => {
                 const submarineMesh = gltf.scene
-                submarineMesh.position.set(0, 10, 0)
+                submarineMesh.position.set(0, -2, 0)
                 submarineMesh.castShadow = true
                 submarineMesh.scale.setScalar(10)
                 this.submarineMesh = submarineMesh
